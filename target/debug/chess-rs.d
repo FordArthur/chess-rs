191b10f8dca5345066438deecf5309473e193ab5
Ford Arthur <jajasaludos.112358@gmail.com>
2024-01-26 16:39:47 +0100

    first commit

---
+++ target/debug/chess-rs.d
@@ -0,0 +1,1 @@
+/home/ford/Projects/chess-rs/target/debug/chess-rs: /home/ford/Projects/chess-rs/src/main.rs
---
+++ target/debug/deps/chess_rs-de9216de7f6df58c.d
@@ -0,0 +1,5 @@
+/home/ford/Projects/chess-rs/target/debug/deps/chess_rs-de9216de7f6df58c: src/main.rs
+
+/home/ford/Projects/chess-rs/target/debug/deps/chess_rs-de9216de7f6df58c.d: src/main.rs
+
+src/main.rs:
